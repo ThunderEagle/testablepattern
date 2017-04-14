5007d2e2570b83aa1c90d652922f79c25beeab54
Williams, Scott <swilliams@tmwsystems.com>
2017-04-14 10:37:41 -0400

    Changes

--- TestablePattern/Views/Foo.cs
+++ TestablePattern/Views/Foo.cs
@@ -5,14 +5,13 @@ using WeakEvent;
 
 namespace TestablePattern.Views
 {
-	public partial class Foo : Form
+	public partial class Foo:Form
 	{
 		public Foo(FooViewModel viewModel)
 		{
 			InitializeComponent();
 			bsFoo.DataSource = viewModel;
 			bsFoo.ResetBindings(false);
-
 			//bind click events into the viewmodel
 			WeakStandardEvent<EventArgs>.AddHandler(btnChange, "Click", viewModel.ChangeText);
 			WeakStandardEvent<EventArgs>.AddHandler(btnShowMessage, "Click", viewModel.DisplayMessage);
@@ -20,9 +19,7 @@ namespace TestablePattern.Views
 			WeakStandardEvent<EventArgs>.AddHandler(btnAddToList, "Click", viewModel.AddToList);
 			WeakStandardEvent<EventArgs>.AddHandler(btnModifyItem, "Click", viewModel.ModifyItem);
 			WeakStandardEvent<EventArgs>.AddHandler(btnSave, "Click", viewModel.SaveFile);
-			
+
 		}
-
-
 	}
 }
